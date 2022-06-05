f258e09f8af5811be5e875856ee2466286b5f54a
Anthony Oleinik <antholeinik@gmail.com>
2022-06-04 22:42:36 -0700

    finally got mf commits working

--- postgraphile/migrations/current.sql
+++ postgraphile/migrations/current.sql
@@ -1,17 +1,1 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA public;
-
-CREATE OR REPLACE FUNCTION trigger_set_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE TABLE IF NOT EXISTS base (
-    id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-
+-- Enter migration here
